c035770560845c7d7bb9c0712ae4855a046913f1
Julius Hartmann <julius.94@hotmail.de>
2019-04-09 22:20:20 +0200

    initial prototype

---
+++ DiceDealerUnity/Assets/Scripts/DiceSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject dicePrefab;
+    [SerializeField] Vector3 randomForcePower;
+    private Transform spawnpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnpoint = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GameObject dice = Instantiate(dicePrefab, spawnpoint.position, Quaternion.identity);
+            dice.GetComponent<Rigidbody>().AddForce(
+                new Vector3(Random.Range(-randomForcePower.x, randomForcePower.x), randomForcePower.y, Random.Range(-randomForcePower.z, randomForcePower.z)), ForceMode.Impulse);
+            dice.GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(Random.Range(-randomForcePower.x, randomForcePower.x), randomForcePower.y, Random.Range(-randomForcePower.z, randomForcePower.z)), ForceMode.Impulse);
+        }
+    }
+}
