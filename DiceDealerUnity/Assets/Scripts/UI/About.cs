6695046c7ab34c69e0dc29c05423457c85c66051
Tobias Fox <tobiasfox@gmx.net>
2019-04-20 17:45:00 +0200

    add about button and change icons

---
+++ DiceDealerUnity/Assets/Scripts/UI/About.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class About : MonoBehaviour
+{
+
+    [SerializeField] private GameObject aboutPanel;
+    public void ChangeAboutPanelVisibility()
+    {
+        aboutPanel.SetActive(!aboutPanel.activeSelf);
+    }
+        
+}
