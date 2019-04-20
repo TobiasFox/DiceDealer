d2194417725f533d54b4d767ef89e31af439cbb4
Tobias Fox <tobiasfox@gmx.net>
2019-04-20 18:23:17 +0200

    update credits and about button

---
+++ DiceDealerUnity/Assets/Scripts/UI/SwitchVisibility.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchVisibility : MonoBehaviour
+{
+    [SerializeField] private GameObject aboutPanel;
+    [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private bool switchAboutPanel;
+
+    public void SwitchVisibilityOfGameObject()
+    {
+        if (switchAboutPanel)
+        {
+            aboutPanel.SetActive(!aboutPanel.activeSelf);
+            creditsPanel.SetActive(false);
+        }
+        else
+        {
+            creditsPanel.SetActive(!creditsPanel.activeSelf);
+            aboutPanel.SetActive(false);
+        }
+    }
+}
