9e915dddea964c3d4e8cf02a618ede43c2d01f8d
Tobias Fox <tobiasfox@gmx.net>
2019-04-24 20:23:12 +0200

    update upgrade price and threshold of shake event

--- DiceDealerUnity/Assets/Scripts/Upgrade.cs
+++ DiceDealerUnity/Assets/Scripts/Upgrade.cs
@@ -7,12 +7,13 @@ public class Upgrade : ScriptableObject
     public string name;
     public int price;
     public float upgradeMultiplier;
-    
+    public float priceMultiplier;
+
     private int upgradeLevel = 0;
 
     internal void CalculateNextUpgradePrice()
     {
-        price += price;
+        price += (int) Math.Ceiling(price * priceMultiplier);
         upgradeLevel++;
     }
 }
