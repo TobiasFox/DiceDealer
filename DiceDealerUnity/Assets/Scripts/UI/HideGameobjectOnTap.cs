daca387120b8c4fe8b821472f761fa6cf30f3e23
Tobias Fox <tobiasfox@gmx.net>
2019-05-21 11:36:14 +0200

    update about and help window

---
+++ DiceDealerUnity/Assets/Scripts/UI/HideGameobjectOnTap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HideGameobjectOnTap : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) || IsTouched())
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private bool IsTouched()
+    {
+        var touchCount = Input.touchCount > 0;
+        if (!touchCount)
+        {
+            return false;
+        }
+
+        var touch = Input.GetTouch(0);
+        return !EventSystem.current.currentSelectedGameObject && touch.phase == TouchPhase.Ended;
+    }
+}
